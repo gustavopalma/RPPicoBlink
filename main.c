053f93ee3466ede22b90996ce3161cf1de94d015
Gustavo Palma <gustavofpalma@gmail.com>
2023-08-12 20:13:12 -0300

    Commit inicial para a Raspberry Pi Pico W

--- main.c
+++ main.c
@@ -1,22 +1,21 @@
 // Inclui a biblioteca padrão do SDK
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
 
-    // cria uma constante com o pino padrão do led
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    
    /*
-    * Inicia o Pino de GPIO e o configura como saída
+    * No Modelo Raspberry Pi Pico W o LED Onboard está ligado
+    * ao Módulo wifi, dessa forma é necessário inicializa-lo 
+    * para então executar o código
     */
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    
+    cyw43_arch_init();
+
     // Loop infinito para o funcionamento do pisca pisca
     while (true) {
-        gpio_put(LED_PIN, 1);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(250);
     }
 }
